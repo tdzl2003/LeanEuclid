1844a87d66624c6461801112d7c9ae39fc089399
DengYun <dengyun@meideng.net>
2025-09-08 16:36:14 +0800

    move between_exists into Order.lean

--- Geometry/Order.lean
+++ Geometry/Order.lean
@@ -9,6 +9,199 @@ import Mathlib.Data.List.Basic
 namespace Geometry.HilbertAxioms1D
   variable {Point: Type}[G: HilbertAxioms1D Point]
 
+
+  /-- 根据公理I.7.2，直线外恒有一点 -/
+  def point_outside_line(l: G.Line)[(p: Point) → Decidable (p ∈ l)]: {p: Point // p ∉ l} :=
+    let ⟨⟨A, B, C⟩, hne, hnc⟩ := G.exists_three_noncollinear_points
+    if hA: A ∈ l then
+      if hB: B ∈ l then
+        have hne: A≠B := by
+          simp only [List.pairwise_cons, List.mem_cons, List.not_mem_nil, or_false,
+            forall_eq_or_imp, forall_eq, IsEmpty.forall_iff, implies_true, List.Pairwise.nil,
+            and_self, and_true] at hne
+          exact hne.1.1
+        have hC: C ∉ l := by
+          intro h
+          apply hnc
+          have : l = G.mk_line hne := by
+            apply G.unique_line_from_two_points hne hA hB
+          rw [collinear_def]
+          use l
+        ⟨C, hC⟩
+      else
+        ⟨B, hB⟩
+    else
+      ⟨A, hA⟩
+
+  theorem lies_on_mk_line_of_between{a b c: Point}(hne: a≠c)(h: Between a b c):
+    b ∈ (mk_line hne).val :=
+  by
+    have h := G.collinear_of_between h
+    rw [collinear_def] at h
+    let ⟨l, ha, hb, hc⟩ := h
+    have : l = mk_line hne := by
+      apply G.unique_line_from_two_points
+      exact ha
+      exact hc
+    rw [← this]
+    exact hb
+
+  def between_exists{a c: Point}(hne: a ≠ c): {b: Point // G.Between a b c} :=
+    -- 直线AC
+    let ⟨l1, ha1, hc1⟩ := G.mk_line hne
+
+    -- 根据公理I.7.2，直线外恒有一点E
+    let ⟨e, he1⟩ := point_outside_line l1
+
+    -- A必不等于E，否则E处在直线AC上
+    have hae: a ≠ e := by
+      intro h
+      apply he1
+      rw [← h]
+      exact ha1
+
+    -- 根据公理II.2.2，直线AE上有一点F，使E在线段AF内
+    let ⟨f, hf1⟩ := G.extension_exists hae
+
+
+    -- F必不等于C，否则F和E都将处在直线AC上
+    have hfc: f ≠ c := by
+      intro h
+      apply he1
+      have : l1 = G.mk_line hne := by
+        apply G.unique_line_from_two_points
+        exact ha1
+        exact hc1
+      rw [this]
+      subst h
+      apply lies_on_mk_line_of_between
+      exact hf1
+
+    let ⟨g, hg1⟩ := G.extension_exists hfc
+
+    have hg2: f ≠ g := by
+      have h := G.between_ne hg1
+      rw [List.pairwise_iff_getElem] at h
+      specialize h 0 2 (by norm_num) (by norm_num) (by decide)
+      simp only [List.getElem_cons_zero, List.getElem_cons_succ] at h
+      exact h
+
+    have hg2: e ≠ g := by
+      have t1 := G.collinear_of_between hf1
+      have t2 := G.collinear_of_between hg1
+      rw [collinear_def] at t1 t2
+      -- 直线AEF
+      let ⟨l3, ha, he, hf1⟩ := t1
+      -- 直线FCG
+      let ⟨l4, hf2, hc, hg⟩ := t2
+
+      intro hc2
+      apply hg2
+      apply common_point_of_lines l3 l4
+      . -- l3 ≠ l4
+        have : c ∉ l3 := by
+          intro h
+          have: l1 = l3 := by
+            have : l1 = mk_line hne := by
+              apply G.unique_line_from_two_points
+              exact ha1
+              exact hc1
+            rw [this]
+            have : l3 = mk_line hne := by
+              apply G.unique_line_from_two_points
+              exact ha
+              exact h
+            rw [this]
+          apply he1
+          rw [this]
+          exact he
+        intro h
+        apply this
+        rw [h]
+        exact hc
+      . exact hf1
+      . exact hf2
+      . rw [← hc2]
+        exact he
+      . exact hg
+
+    -- 直线EG
+    let ⟨l2, hl2⟩ := G.mk_line hg2
+
+    -- TODO: 和上述证明有少许重复，看看如何精简
+    have t1 : ¬ Collinear a f c := by
+      intro h
+      have t1 := G.collinear_of_between hf1
+      rw [collinear_def] at t1 h
+      -- 直线AEF
+      let ⟨l3, ha, he, hf2⟩ := t1
+      -- 虚构的矛盾直线ACF
+      let ⟨l4, ha', hf', hc'⟩ := h
+
+      have hf3 : f ≠ a := by
+        have hf1 := G.between_ne hf1
+        rw [List.pairwise_iff_getElem] at hf1
+        specialize hf1 0 2 (by norm_num) (by norm_num) (by decide)
+        simp only [List.getElem_cons_zero, List.getElem_cons_succ] at hf1
+        apply Ne.symm hf1
+
+      apply he1
+      -- l3、l4共点a、f，所以相等
+      have t2: l3 = l4 := by
+        have: l3 = mk_line hf3 := by
+          apply G.unique_line_from_two_points
+          exact hf2
+          exact ha
+        rw [this]
+        have: l4 = mk_line hf3 := by
+          apply G.unique_line_from_two_points
+          exact hf'
+          exact ha'
+        rw [this]
+      -- l1、l3共点a、c，所以相等
+      have t3: l1 = l3 := by
+        have : l1 = mk_line hne := by
+          apply G.unique_line_from_two_points
+          exact ha1
+          exact hc1
+        rw [this]
+        have : l3 = mk_line hne := by
+          apply G.unique_line_from_two_points
+          exact ha
+          rw [t2]
+          exact hc'
+        rw [this]
+      rw [t3]
+      exact he
+
+    have t2 : ∃ P: Point, OnSegment a P f ∧ P ∈ l2 := by
+      use e
+      and_intros
+      . rw [OnSegment_def]
+        apply Or.inl
+        exact hf1
+      . exact hl2.1
+
+    have ⟨b, hb1, hb2⟩  := G.pasch_axiom t1 t2
+
+    have hFinal: Between a b c  := by
+      have t1: ¬ OnSegment f b c := by
+        -- 如果b在fc之间，那么b=g，但g又不在fc之间
+        sorry
+      have t2: b ≠ a := by
+        -- 如果b=a，那么e=a，矛盾
+        sorry
+
+      have t3: b ≠ c := by
+        -- 可以直接从t1推出
+        simp only [OnSegment_def, not_or] at t1
+        exact t1.2.2
+      simp only [t1, t2, t3, OnSegment_def, false_or, or_false] at hb1
+      exact hb1
+
+    ⟨b, hFinal⟩
+
+
   /-- theorem II.3 Of any three points situated on a straight line, there is always one and only one which lies Between the other two. -/
   theorem between_trichotomy(a b c: Point): a ≠ b → b ≠ c → a ≠ c →
       (G.Between a b c ∨ G.Between b a c ∨ G.Between a c b) ∧
